266768caec6ce50452b57abade472b4bb73ad36c
shari1210 <76665608+shari1210@users.noreply.github.com>
2023-07-12 13:12:16 +0200

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,4 +1,13 @@
 import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: "/webapp",  
+  title: "Simple VitePress Site",
+  description: "Training Page for Cloud Computing Course",
+...
+
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
