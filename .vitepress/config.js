e4c05a74884d8c44224390a9a1e4f077777c25d3
shari1210 <76665608+shari1210@users.noreply.github.com>
2023-07-12 13:16:53 +0200

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -6,3 +6,31 @@ export default defineConfig({
   title: "Simple VitePress Site",
   description: "Training Page for Cloud Computing Course",
 ...
+
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Simple VitePress Site",
+  description: "Training Page for Cloud Computing Course",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
